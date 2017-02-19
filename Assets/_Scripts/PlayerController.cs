212502a65b9772d508304ef45ad3dcf5f60901a4
Wesley Tseng <bionicle_1121@hotmail.com>
2017-02-19 00:32:02 -0800

    Updated Art Asset and Animation
    Created Skeleton Sprite Sheet
    Got basic walking animation for Knight
    Creating small section of map

--- Assets/_Scripts/PlayerController.cs
+++ Assets/_Scripts/PlayerController.cs
@@ -3,14 +3,37 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
+    Animator animate;
 
-	// Use this for initialization
-	void Start () {
-		
+    // Use this for initialization
+    void Start () {
+
+        animate = GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        Movement();
+
+        {
+            float move = Input.GetAxis("Horizontal");
+            animate.SetFloat("Speed", move);
+        }
 	}
+
+    void Movement() {
+        if (Input.GetKey (KeyCode.D)) {
+            transform.Translate(Vector2.right * 5f * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(-Vector2.right * 5f * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.Space) || Input.GetKey(KeyCode.W))
+        {
+
+        }
+    }
 }
